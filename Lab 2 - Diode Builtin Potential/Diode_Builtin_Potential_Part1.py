8171d12c7b73e2194e79618a07ce4bd94d9de7b1
Stockfalcon <thomas.murimboh@gmail.com>
2025-05-02 13:08:11 -0300

    Diode project base works!

--- Lab 2 - Diode Builtin Potential/Diode_Builtin_Potential_Part1.py
+++ Lab 2 - Diode Builtin Potential/Diode_Builtin_Potential_Part1.py
@@ -1,8 +1,12 @@
 import nidaqmx
 from nidaqmx.constants import TerminalConfiguration, AcquisitionType, Edge
+from nidaqmx.stream_readers import AnalogMultiChannelReader
+from nidaqmx.stream_writers import AnalogSingleChannelWriter
 import numpy as np
-import scipy 
+import threading 
 import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import animation
 
 # ----- MISTAKES(?) IN LAST WEEK'S LAB -----
 
@@ -15,16 +19,11 @@ import pandas as pd
 # Some one with more competency than me should look here to verify if I'm correct
 # https://www.ni.com/docs/en-US/bundle/ni-daqmx/page/refsingleended.html
 
-# ----- NOTE -----
-#I use the terms Leader and Followers to describe each channel, but they are more commonly refered to as Master and Slaves
-
 
 device = 'Dev2'
 resistor_channel = 'ai0'
 diode_channel = 'ai1'
 voltage_channel ='ao0'
-# SampleClock = f"{device}/ao1/SampleClock"
-# trigger_line = f"{device}/ao1/StartTrigger"
 
 SampleClock = "Dev2/ao/SampleClock"
 trigger_line = "Dev2/ao/StartTrigger"
@@ -36,56 +35,90 @@ samples_per_point = 50 # we will be taking the average of this many samples for 
 total_time = (samples_per_point * total_data_points) / sample_rate # total time in seconds
 ao_voltages = np.linspace(-10,10,total_data_points)
 
+all_data = [[]] # an empy 2D array
+data = np.zeros((2,samples_per_point))
+plot_buffer = np.zeros((2,1000)) # rolling window
+
+take_readings=True
 
 #NOTE TO SELF: WHEN READINGS ARE TAKEN, THEY WILL TAKE A NUMBER OF SAMPLES DEFINED BY samples_per_point  
 
-with nidaqmx.Task() as task_resistor_ai, nidaqmx.Task() as task_diode_ai, nidaqmx.Task() as task_ao:
-    # set up all resistor analog input. This will be a follower which listens for a trigger, and timing signals to take readings 
-    task_resistor_ai.ai_channels.add_ai_voltage_chan(f'{device}/{resistor_channel}',
-                                                    terminal_config=TerminalConfiguration.DIFF) # add an ai channel and set the measurement type to an differential measurement system.
-    #                                                                                           This ensures that the input measured is NOT referenced to ground, but to the negative ai0 terminal.  
-    #                                                                                           https://www.ni.com/docs/en-US/bundle/ni-daqmx/page/refsingleended.html
-    task_resistor_ai.timing.cfg_samp_clk_timing(sample_rate, 
-                                                source=SampleClock,
-                                                sample_mode=AcquisitionType.FINITE,
-                                                samps_per_chan=samples_per_point)
-    task_resistor_ai.triggers.start_trigger.cfg_dig_edge_start_trig(trigger_source=trigger_line,
-                                                                    trigger_edge=Edge.RISING)
+def nidaqmx_task():
+    global data
 
-    # setup diode analog input. This will be a follower which listens for a trigger, and timing signals to take readings 
-    task_diode_ai.ai_channels.add_ai_voltage_chan(f'{device}/{diode_channel}',terminal_config=TerminalConfiguration.DIFF) 
-    task_diode_ai.timing.cfg_samp_clk_timing(sample_rate, 
-                                                source=SampleClock,
-                                                sample_mode=AcquisitionType.FINITE,
-                                                samps_per_chan=samples_per_point) 
-    task_diode_ai.triggers.start_trigger.cfg_dig_edge_start_trig(trigger_source=trigger_line,
-                                                                    trigger_edge=Edge.RISING)
+    with nidaqmx.Task() as task_ai,  nidaqmx.Task() as task_ao:
+        # set up all resistor analog input. This will be a follower which listens for a trigger, and timing signals to take readings 
+        task_ai.ai_channels.add_ai_voltage_chan(f'{device}/{resistor_channel}',
+                                                        terminal_config=TerminalConfiguration.DIFF) # add an ai channel and set the measurement type to an differential measurement system.
+        #                                                                                           This ensures that the input measured is NOT referenced to ground, but to the negative ai0 terminal.  
+        #                                                                                           https://www.ni.com/docs/en-US/bundle/ni-daqmx/page/refsingleended.html
+        '''
+        task_resistor_ai.timing.cfg_samp_clk_timing(sample_rate, 
+                                                    source=SampleClock,
+                                                    sample_mode=AcquisitionType.FINITE,
+                                                    samps_per_chan=samples_per_point)
+        '''
+        
+        # setup diode analog input. This will be a follower which listens for a trigger, and timing signals to take readings 
+        task_ai.ai_channels.add_ai_voltage_chan(f'{device}/{diode_channel}',terminal_config=TerminalConfiguration.DIFF) 
+        '''
+        task_diode_ai.timing.cfg_samp_clk_timing(sample_rate, 
+                                                    source=SampleClock,
+                                                    sample_mode=AcquisitionType.FINITE,
+                                                    samps_per_chan=samples_per_point) 
+        
+        task_ai.triggers.start_trigger.cfg_dig_edge_start_trig(trigger_source=trigger_line,
+                                                                trigger_edge=Edge.RISING)         
+        '''
+        # setup analog outpu. This will be the Leader for sending triggers and telling the Followers when to take readings
+        task_ao.ao_channels.add_ao_voltage_chan(f"{device}/{voltage_channel}",min_val=-10, max_val=10)
+        '''
+        task_ao.timing.cfg_samp_clk_timing(sample_rate, 
+                                            source=SampleClock,
+                                            sample_mode=AcquisitionType.FINITE,
+                                            samps_per_chan=samples_per_point)
+        
+        task_ao.triggers.start_trigger.disable_start_trig() # this is to tell the Leader that it's job is to make a start signal and not to wait until it receives one
+        '''
+        reader = AnalogMultiChannelReader(task_ai.in_stream)
+        writer = AnalogSingleChannelWriter(task_ao.in_stream)
 
-    # setup analog outpu. This will be the Leader for sending triggers and telling the Followers when to take readings
-    task_ao.ao_channels.add_ao_voltage_chan(f"{device}/{voltage_channel}",min_val=-10, max_val=10)
-    task_ao.timing.cfg_samp_clk_timing(sample_rate, 
-                                        source=SampleClock,
-                                        sample_mode=AcquisitionType.FINITE,
-                                        samps_per_chan=samples_per_point)
-    task_ao.triggers.start_trigger.disable_start_trig() # this is to tell the Leader that it's job is to make a start signal and not to wait until it receives one
+        task_ai.start()
+        task_ao.start()
+        writer.write_many_sample(ao_voltages)
+
+        while take_readings:
+            reader.read_many_sample(data, samples_per_point,timeout=10.0)
+             # Shift old data and add new samples for plotting (NOT THREAD SAFE!)
+            plot_buffer[:, :-samples_per_point] = plot_buffer[:, samples_per_point:]
+            plot_buffer[:, -samples_per_point:] = data
 
 
+t=threading.Thread(target=nidaqmx_task)
 
-    task_ao.write(voltage_channel, auto_start=False)
+t.start()
 
-    # Start the Followers before the Leader so that they don't miss the Leader's trigger signal
-    task_resistor_ai.start()
-    task_diode_ai.start()
-    task_ao.start()
+fig, ax = plt.subplots()
+lines = [ax.plot(plot_buffer[i])[0] for i in range(2)]
+ax.set_ylim(-0.01),0.1
 
-    resistor_data = task_resistor_ai.read()
-    diode_data = task_diode_ai.read()
+def update(frame):
+    for i, line in enumerate(lines):
+        line.set_ydata(plot_buffer[i])
+    return lines
 
-resistor_data=np.array(resistor_data)
-diode_data=np.array(diode_data)
 
-df = pd.DataFrame([resistor_data,diode_data])
-print(df)
+ani = animation.FuncAnimation(fig, update, interval=50, blit=True)
+
+
+plt.show()
+take_readings = False
+t.join() # wait for all threads to finnish
+print(data)
+
+
+            
+
 
 
     # for i in range(len(ao_voltages)):# this is currently software controlled data aquisition, later we will make this hardware controlled (faster and therefore better)
